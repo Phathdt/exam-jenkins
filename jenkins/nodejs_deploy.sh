c4d6c63be7995347def717f04c2ab9e8d5005d64
Phathdt <phathdt379@gmail.com>
2020-09-19 16:10:37 +0700

    update deployment

--- jenkins/nodejs_deploy.sh
+++ jenkins/nodejs_deploy.sh
@@ -4,6 +4,6 @@ ssh slave
 
 docker pull ${DOCKER_NODEJS_IMAGE}:latest
 
-docker stop nodejs || true && docker rm nodejs || true
+docker rm -f nodejs || true
 
 docker run --name nodejs -d -p 3000:3000 -e APP_VERSION="lastest" -e HOST_NAME="Phathdts-MacBook-Pro.local" ${DOCKER_NODEJS_IMAGE}:latest
--- jenkins/python_deploy.sh
+++ jenkins/python_deploy.sh
@@ -4,6 +4,6 @@ ssh slave
 
 docker pull ${DOCKER_PYTHON_IMAGE}:latest
 
-docker stop python || true && docker rm python || true
+docker rm -f nodejs || true
 
 docker run --name python -d -p 5000:5000 -e APP_VERSION="lastest" -e HOST_NAME="Phathdts-MacBook-Pro.local" ${DOCKER_PYTHON_IMAGE}:latest
