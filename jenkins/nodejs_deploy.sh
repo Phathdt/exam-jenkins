a73897070430b108c23956afa3646d234cc8cb34
Phathdt <phathdt379@gmail.com>
2020-09-19 16:22:56 +0700

    update deployment

--- jenkins/nodejs_deploy.sh
+++ jenkins/nodejs_deploy.sh
@@ -4,6 +4,14 @@ ssh slave
 
 docker pull ${DOCKER_NODEJS_IMAGE}:latest
 
-docker rm -f nodejs || true
+CONTAINER=nodejs
+
+RUNNING=$(docker inspect --format="{{ .State.Running }}" $CONTAINER 2> /dev/null)
+
+if [ $? -eq 1 ]; then
+    echo "'$CONTAINER' does not exist."
+else
+    /usr/bin/docker rm --force $CONTAINER
+fi
 
 docker run --name nodejs -d -p 3000:3000 -e APP_VERSION="lastest" -e HOST_NAME="Phathdts-MacBook-Pro.local" ${DOCKER_NODEJS_IMAGE}:latest
--- jenkins/python_deploy.sh
+++ jenkins/python_deploy.sh
@@ -4,6 +4,14 @@ ssh slave
 
 docker pull ${DOCKER_PYTHON_IMAGE}:latest
 
-docker rm -f nodejs || true
+CONTAINER=python
+
+RUNNING=$(docker inspect --format="{{ .State.Running }}" $CONTAINER 2> /dev/null)
+
+if [ $? -eq 1 ]; then
+    echo "'$CONTAINER' does not exist."
+else
+    /usr/bin/docker rm --force $CONTAINER
+fi
 
 docker run --name python -d -p 5000:5000 -e APP_VERSION="lastest" -e HOST_NAME="Phathdts-MacBook-Pro.local" ${DOCKER_PYTHON_IMAGE}:latest
